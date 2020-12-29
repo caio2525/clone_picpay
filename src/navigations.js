9839e5a59489c80b51ba42e6df0c14403216031e
caio fernandes lima <caio.fernandeslima25@gmail.com>
2020-12-29 16:28:56 -0500

    added icons

--- src/navigations.js
+++ src/navigations.js
@@ -5,26 +5,93 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import home from './screens/home';
 import wallet from './screens/wallet';
 import pay from './screens/pay';
+import { FontAwesome, Entypo, AntDesign, Ionicons, Feather } from '@expo/vector-icons';
+
+const icons = {
+  home:{
+    lib: Entypo,
+    name: 'home'
+  },
+
+  wallet:{
+    lib: AntDesign,
+    name: 'creditcard'
+  },
+
+  pay:{
+    lib: FontAwesome,
+    name: 'dollar'
+  },
+
+  notifications:{
+    lib: Ionicons,
+    name: 'notifications'
+  },
+
+  settings:{
+    lib: Feather,
+    name: 'settings'
+  }
+}
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigations() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          const {lib: Icon, name} = icons[route.name];
+          return (<Icon name={name} size={size} color={color}/>)
+        }
+      })}
+    >
       <Tab.Screen
         name='home'
         component={home}
+        options={
+          {
+            title: 'home'
+          }
+        }
       />
 
       <Tab.Screen
         name='wallet'
         component={wallet}
+        options={{
+          title: 'wallet'
+        }}
       />
 
       <Tab.Screen
         name='pay'
         component={pay}
+        options={{
+          title: 'pay'
+        }}
       />
+
+      <Tab.Screen
+        name='notifications'
+        component={home}
+        options={
+          {
+            title: 'notifications'
+          }
+        }
+      />
+
+      <Tab.Screen
+        name='settings'
+        component={home}
+        options={
+          {
+            title: 'settings'
+          }
+        }
+      />
+
     </Tab.Navigator>
   );
 }
